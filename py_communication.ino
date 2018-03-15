c946de7b6586a020a6e55b16febdb4864810af08
Navin Poudel <37016579+da-one@users.noreply.github.com>
2018-03-15 12:21:11 +0545

    Add files via upload

---
+++ py_communication.ino
@@ -0,0 +1,25 @@
+int LED=13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED, LOW);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0)
+  {
+    if(Serial.read()=='s')
+    {
+      digitalWrite(LED, HIGH);
+      delay(2000);
+    }
+    else
+    {
+      digitalWrite(LED, LOW);
+    }
+  }
+
+}
